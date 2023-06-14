805fc441a7b20eedaf02137b64aa204a4cd26888
Carlo Dee <carlodee.official@proton.me>
2023-06-14 14:31:42 +0800

    Create messi.sh
    Bash Script for cloning important sources.

---
+++ messi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Source Directory
+cd ~/android/lineage
+
+# Device Tree
+git clone https://github.com/cd-Crypton/android_device_realme_messi.git -b thirteen ./device/realme/messi
+
+# Vendor Tree
+git clone https://github.com/cd-Crypton/proprietary_tree_realme_messi.git -b 13 ./vendor/realme/messi
+
+# Firmware Tree
+git clone https://gitlab.com/firmware-dump/vendor_firmware_realme.git -b 13 ./vendor/firmware
+
+# Kernel Tree
+git clone https://github.com/cd-Crypton/android_kernel_realme_messi.git -b messi-12.3-1 ./kernel/realme/messi
+
+# Hardware OPlus
+git clone https://github.com/cd-Crypton/android_hardware_oplus.git -b lineage-20 ./hardware/oplus
